d80070a83ced4e78ef3afe48976dd603de473bf9
lalibre <laura.osorio.ovalle@gmail.com>
2025-08-07 19:40:27 +0200

    Bitacora terminada
    Bitacora terminada

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -4,12 +4,13 @@ class_name GameManager
 var puntuacion := 0
 var score_label: Label
 var log_panel: RichTextLabel
+var bitacora_texto := ""
 
 func _ready():
 	var scene_root = get_tree().get_root()
 
 	score_label = scene_root.get_node_or_null("GameScene_ComputerOff/UIX/InfoPanel/VBoxContainer/ScoreLabel")
-	log_panel = scene_root.get_node_or_null("GameScene_ComputerOff/UIX/InfoPanel/VBoxContainer/LogPanel")
+	log_panel = scene_root.get_node_or_null("GameScene_ComputerOff/UIX/InfoPanel/VBoxContainer/ScrollContainer/LogPanel")
 
 	print("score_label:", score_label)
 	print("log_panel:", log_panel)
@@ -26,11 +27,35 @@ func sumar_puntos(valor: int):
 func registrar_accion(texto: String):
 	if log_panel:
 		log_panel.append_text("🕵️ " + texto + "\n")
-
+		var entrada := texto + "\n"
+		bitacora_texto += entrada
+		
 func registrar_en_bitacora(mensaje: String) -> void:
+	var timestamp = "[" + Time.get_datetime_string_from_system().substr(11, 8) + "]"  # Solo HH:MM:SS
+	var entrada = "[color=blue]" + timestamp + "[/color] " + mensaje
+	
 	if log_panel:
-		log_panel.append_text("- " + mensaje + "\n")
-		log_panel.append_text("📝 Bitácora inicializada...\n")
-		print(log_panel)
+		if log_panel is RichTextLabel:
+			print("¡Es un RichTextLabel!")
+		else:
+			print("❌ No es un RichTextLabel.")
+		log_panel.append_text(entrada + "\n")
+		log_panel.scroll_to_line(log_panel.get_line_count() - 1)
 	else:
 		print("❌ log_panel no está enlazado correctamente.")
+		
+func guardar_bitacora_en_archivo():
+	if not log_panel:
+		print("Error: log_panel no encontrado.")
+		return
+
+	var ruta = "user://bitacora_forense.txt"
+	var archivo = FileAccess.open(ruta, FileAccess.WRITE)
+	if archivo:
+		archivo.store_string(bitacora_texto)
+		archivo.close()
+		print("Contenido de bitácora:", bitacora_texto)
+
+		print("Bitácora guardada en:", ruta)
+	else:
+		print("No se pudo guardar la bitácora.")
